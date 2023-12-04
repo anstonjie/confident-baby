d6388f5c231b123f75b65c7a93c67a4d60f26c9b
jianweijie <molijiqiren@163.com>
2023-12-04 15:38:34 +0800

    323

---
+++ pages/test/test.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<button>测试1</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
