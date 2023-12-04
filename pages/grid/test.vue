2d578fc9d98b06ca45555e4a92ac028d72091c96
jianweijie <molijiqiren@163.com>
2023-12-04 15:38:24 +0800

    122

---
+++ pages/grid/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>
+    <button>测试2</button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
